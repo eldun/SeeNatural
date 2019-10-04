928411eac652427d49dc20c189a80c56077fc60c
dunneev <dunneev@gmail.com>
2019-10-04 13:38:05 -0400

    Add clef and difficulty views

--- app/src/main/java/com/dunneev/seenatural/MainActivity.java
+++ app/src/main/java/com/dunneev/seenatural/MainActivity.java
@@ -2,11 +2,14 @@ package com.dunneev.seenatural;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES); // TODO: Add a shared pref to select light or dark mode for all screens
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }

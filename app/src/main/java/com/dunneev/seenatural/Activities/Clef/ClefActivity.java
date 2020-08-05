e953a74ba7cf2701000f6142a8130ff94706b2d7
dunneev <dunneev@gmail.com>
2020-08-05 18:48:48 -0400

    Add native cpp support (remove example textbox)

--- app/src/main/java/com/dunneev/seenatural/Activities/Clef/ClefActivity.java
+++ app/src/main/java/com/dunneev/seenatural/Activities/Clef/ClefActivity.java
@@ -1,21 +1,20 @@
 package com.dunneev.seenatural.Activities.Clef;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AppCompatDelegate;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 import com.dunneev.seenatural.Activities.Difficulty.DifficultyActivity;
 import com.dunneev.seenatural.R;
 
 public class ClefActivity extends AppCompatActivity {
 
-    static {
-        System.loadLibrary("native-lib");
-    }
+//    static {
+//        System.loadLibrary("native-lib");
+//    }
 
     public static final String EXTRA_SELECTED_CLEF =
             "com.dunneev.seenatural.ClefActivity.extra.SELECTED_CLEF";
@@ -31,8 +30,8 @@ public class ClefActivity extends AppCompatActivity {
         setContentView(R.layout.activity_clef);
 
         // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        // TextView tv = (TextView) findViewById(R.id.sample_text);
+        // tv.setText(stringFromJNI());
     }
 
     public void launchDifficultyActivity(View view) {
@@ -42,9 +41,9 @@ public class ClefActivity extends AppCompatActivity {
         startActivity (intent);
     }
 
-    /**
+    /*
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    public native String stringFromJNI();
+//    public native String stringFromJNI();
 }

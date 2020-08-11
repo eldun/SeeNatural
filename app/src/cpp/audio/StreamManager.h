bd6d0efd752a737e53f89a8033c60b331c45a57d
dunneev <dunneev@gmail.com>
2020-08-10 23:29:47 -0400

    Remove sine wave variables

--- app/src/cpp/audio/StreamManager.h
+++ app/src/cpp/audio/StreamManager.h
@@ -17,14 +17,6 @@ private:
     // Stream params
     static int constexpr kChannelCount = 2;
     static int constexpr kSampleRate = 48000;
-    // Wave params, these could be instance variables in order to modify at runtime
-    static float constexpr kAmplitude = 0.5f;
-    static float constexpr kFrequency = 440;
-    static float constexpr kPI = M_PI;
-    static float constexpr kTwoPi = kPI * 2;
-    static double constexpr mPhaseIncrement = kFrequency * kTwoPi / (double) kSampleRate;
-    // Keeps track of where the wave is
-    float mPhase = 0.0;
 };
 
 #endif //SEENATURAL_STREAMMANAGER_H

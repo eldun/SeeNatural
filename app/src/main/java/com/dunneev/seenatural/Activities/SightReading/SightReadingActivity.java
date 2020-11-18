4fe18a4b941f4b535907d536fbf8f187db23088e
dunneev <dunneev@gmail.com>
2020-11-18 13:04:33 -0500

    Scroll to next note on correct key press

--- app/src/main/java/com/dunneev/seenatural/Activities/SightReading/SightReadingActivity.java
+++ app/src/main/java/com/dunneev/seenatural/Activities/SightReading/SightReadingActivity.java
@@ -204,7 +204,7 @@ public class SightReadingActivity extends AppCompatActivity implements PianoKey.
 
         markNoteCorrect(key.getNote());
 
-        staffView.scrollView.smoothScrollTo(staffView.scrollView.getScrollX() + 150, 0);
+        staffView.scrollToNextNote();
 
         addSightReadingNote(generatePracticablePianoNote());
     }

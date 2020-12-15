978799786a05ccc5b03651182b5f0088fc4bf737
dunneev <dunneev@gmail.com>
2020-12-15 15:46:23 -0500

    Clean up ReadingViewModel

--- app/src/main/java/com/dunneev/seenatural/Fragments/Reading/ReadingViewModel.java
+++ app/src/main/java/com/dunneev/seenatural/Fragments/Reading/ReadingViewModel.java
@@ -12,29 +12,7 @@ import java.util.Random;
 
 public class ReadingViewModel extends ViewModel {
 
-    public PianoNote currentNote;
     public boolean isSingleOctaveMode;
-    public MutableLiveData<Boolean> correctKeyPressed = new MutableLiveData<>();
 
-    public void incorrectKeyPressed() {
-        correctKeyPressed.setValue(false);
-    }
 
-    public void correctKeyPressed() {
-        correctKeyPressed.setValue(true);
-    }
-
-    public void keyDown(PianoNote note) {
-
-        if (note.equals(currentNote, isSingleOctaveMode)){
-            correctKeyPressed();
-        }
-        else {
-            incorrectKeyPressed();
-        }
-    }
-
-    public void keyUp(PianoNote note) {
-        correctKeyPressed.setValue(null);
-    }
 }

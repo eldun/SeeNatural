95c3425c598c2322ea19257705532d66e686601d
dunneev <dunneev@gmail.com>
2021-02-12 23:17:33 -0500

    Add start button to ReadingSetupFragment

--- app/src/main/java/com/dunneev/seenatural/Fragments/ReadingSetupFragment.java
+++ app/src/main/java/com/dunneev/seenatural/Fragments/ReadingSetupFragment.java
@@ -1,6 +1,7 @@
 package com.dunneev.seenatural.Fragments;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,13 +9,17 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.preference.PreferenceFragmentCompat;
 
 import com.dunneev.seenatural.R;
-import com.dunneev.seenatural.databinding.FragmentReadingTabBinding;
+import com.dunneev.seenatural.databinding.FragmentReadingSetupBinding;
 
-public class ReadingTabFragment extends Fragment {
+public class ReadingSetupFragment extends Fragment {
+    private final String LOG_TAG = this.getClass().getSimpleName();
 
-    private FragmentReadingTabBinding binding;
+    private FragmentReadingSetupBinding binding;
+
+
 
     @Override
     public View onCreateView(
@@ -30,13 +35,13 @@ public class ReadingTabFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-//        binding.buttonToPianoFragment.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                NavHostFragment.findNavController(ReadingTabFragment.this)
-//                        .navigate(R.id.);
-//            }
-//        });
+        binding.readingStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(ReadingSetupFragment.this)
+                        .navigate(R.id.readingFragment);
+            }
+        });
 //
 //        binding.buttonToReadingFragment.setOnClickListener(new View.OnClickListener() {
 //            @Override
